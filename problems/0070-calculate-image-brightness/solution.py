21f7d978c0c2bbeb02533759b937c9452e4d4026
Nishant-9696 <242653692+Nishant-9696@users.noreply.github.com>
2026-09-19 17:29:08 +0000

    Update: Calculate Image Brightness

--- problems/0070-calculate-image-brightness/solution.py
+++ problems/0070-calculate-image-brightness/solution.py
@@ -5,13 +5,11 @@ def calculate_brightness(img):
     if len(img) <= 0:
         return -1
 
-    # Check row lengths BEFORE np.array()
     if not all(len(r) == len(img[0]) for r in img):
         return -1
 
     i = np.array(img)
 
-    # Check pixel values
     if np.any(i < 0) or np.any(i > 255):
         return -1
 
